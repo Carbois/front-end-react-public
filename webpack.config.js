3ce489cc112b5af53afa42102e4b4d8696c81973
mk-045 <139829330+mk-045@users.noreply.github.com>
2024-02-20 09:59:29 -0500

    made code in to deployable js package

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/App.js', // Path to your main JS file
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
