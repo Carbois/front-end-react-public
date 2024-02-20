c80276c432a5fc5e737363edf34400e8f1aee599
mk-045 <139829330+mk-045@users.noreply.github.com>
2024-02-20 16:07:44 -0500

    built in dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/App.js', // Path to your main JS file
+  mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
