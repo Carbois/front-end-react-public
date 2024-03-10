76d20dca29b5bc0a1e54083ebf08800c57564065
mk-045 <139829330+mk-045@users.noreply.github.com>
2024-03-10 09:26:12 -0700

    added logic to verififed and premier dealer badges

--- src/App.js
+++ src/App.js
@@ -17,12 +17,18 @@ function Card(props) {
                 <p className="exterior-color"><strong>外观颜色:</strong> {props.exterior_color}</p>
                 <p className="interior-color"><strong>内饰颜色:</strong> {props.interior_color}</p>
                 <p className="Region"><strong>地区:</strong> {props.region}</p>
-                <div className="verified-badge" style={{ fontSize: '18px', display: 'inline-block', backgroundColor: 'green', borderRadius: '50px', padding: '2px 15px' }}>
-                    <p style={{ color: 'white', margin: 0 }}>认证卖家</p>
-                </div>
+                {/* hide the verified badge if the dealer is not verified */}
+                {props.isVerified && (
+                    <div className="verified-badge" style={{ fontSize: '18px', display: 'inline-block', backgroundColor: 'green', borderRadius: '50px', padding: '2px 15px' }}>
+                        <p style={{ color: 'white', margin: 0 }}>认证卖家</p>
+                    </div>
+                )}
+                {/* hide the is_premier_dealer badge if the seller is not a premier_dealer */}
+                {props.is_premier_dealer && (
                 <div className="premier-dealer-badge" style={{ fontSize: '18px', display: 'inline-block', backgroundColor: 'gold', borderRadius: '50px', padding: '2px 15px' }}>
                     <p style={{ color: 'black', margin: 0 }}>精选经销商</p>
                 </div>
+                )}
                 <div className="carfax-badge" style={{ fontSize: '18px', display: 'inline-block', borderRadius: '50px', padding: '10px 0' }}>
                     <img src="https://uploads-ssl.webflow.com/654d8bd9b929e284fbe484ae/65c248a7f51659032c9af01c_carfax_logo.png" style={{ width: '120px' }} />
                 </div>
